99b5542bb302f94c0bcd5d689c0f4b50d2c6f203
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-03-22 15:23:44 -0400

    charging

--- charging_plots.py
+++ charging_plots.py
@@ -19,7 +19,7 @@ eastern = timezone('Canada/Eastern')
 
 def analyze_charge(list):
     last_time = None
-    for entry in list['sensor_data']:
+    for entry in list:
         time = DateTime(entry["time"])
         if last_time is None or (time - last_time).total_seconds() > 3600:
             yield {"time": str(time), "voltage":entry["voltage"], "temp":entry["battery_temperature"]}
