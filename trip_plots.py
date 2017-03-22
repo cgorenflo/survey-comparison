662b646e280eb0189b60eed67a67c80a35e016ec
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-03-22 01:12:57 -0400

    timezone

--- trip_plots.py
+++ trip_plots.py
@@ -2,18 +2,20 @@ from iss4e.db import mysql
 from iss4e.util.config import load_config
 import pandas as pd
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pytz import timezone
 
-
 config = load_config()
 
 participants = pd.read_excel("participant map.xlsx", header=[1])
-female_staff = participants.iloc[1:8][["Email","IMEI"]]
-male_staff = participants.iloc[10:16][["Email","IMEI"]]
-female_students = participants.iloc[18:23][["Email","IMEI"]]
-male_students = participants.iloc[25:32][["Email","IMEI"]]
+female_staff = participants.iloc[1:8][["Email", "IMEI"]]
+male_staff = participants.iloc[10:16][["Email", "IMEI"]]
+female_students = participants.iloc[18:23][["Email", "IMEI"]]
+male_students = participants.iloc[25:32][["Email", "IMEI"]]
+
+eastern = timezone('Canada/Eastern')
 
 
 def get_trips(l):
@@ -21,7 +23,8 @@ def get_trips(l):
     for imei in l["IMEI"]:
         cursor.execute("SELECT start,end from trips where imei={imei}".format(imei=imei))
         result = cursor.fetchall()
-        trips += [(start.replace(tzinfo=timezone('Canada/Eastern')), end.replace(tzinfo=timezone('Canada/Eastern'))) for (start,end) in result]
+        trips += [(start.replace(tzinfo=timezone('UTC')).astimezone(eastern),
+                   end.replace(tzinfo=timezone('UTC')).astimezone(eastern)) for (start, end) in result]
 
     return trips
 
@@ -40,15 +43,15 @@ with mysql.connect(**config["webike.mysql"]) as mysql_client:
     students = fstu_trips + mstu_trips
 
     fig1 = plt.figure()
-    plt.hist([[start.hour for (start,end) in ftrips],[start.hour for (start, end) in mtrips]],24, normed=True)
+    plt.hist([[start.hour for (start, end) in ftrips], [start.hour for (start, end) in mtrips]], 24, normed=True)
     plt.savefig("trip_start_by_gender.png")
 
     fig2 = plt.figure()
-    plt.hist([[start.hour for (start,end) in staff],[start.hour for (start, end) in students]],24, normed=True)
+    plt.hist([[start.hour for (start, end) in staff], [start.hour for (start, end) in students]], 24, normed=True)
     plt.savefig("trip_start_by_occupation.png")
 
     fig3 = plt.figure()
-    plt.hist([start.hour for (start, end) in ftrips+mtrips], 24, normed=True)
+    plt.hist([start.hour for (start, end) in ftrips + mtrips], 24, normed=True)
     plt.savefig("trip_start_all.png")
 
     fig4 = plt.figure()
