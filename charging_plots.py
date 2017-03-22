8329bca9d2dfdb77ceb816d73f4d68fe0b91b877
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-03-22 14:15:32 -0400

    charging

--- charging_plots.py
+++ charging_plots.py
@@ -36,8 +36,10 @@ def get_charging(l):
             (charging_current>30 or (discharge_current < 490 and discharge_current >0) ) limit 3".format(imei=int(imei))
         print(query)
         result = influx_client.stream_query(query)
+        print(result)
         charges += [analyze_charge(entry) for entry in result]
-        print("----------------------------")
+        global last_entry
+        last_entry = None
     return charges
 
 
