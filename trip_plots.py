a6c00c5872cca0380852c12c88eb6dc5d627a450
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-03-26 16:57:17 -0400

    legends

--- trip_plots.py
+++ trip_plots.py
@@ -54,19 +54,21 @@ with mysql.connect(**config["webike.mysql"]) as mysql_client:
     print("avg no. trips female: {trips}".format(trips=str(len(ftrips) / (len(female_staff) + len(female_students)))))
     print("avg no. trips staff: {trips}".format(trips=str(len(staff) / (len(female_staff) + len(male_staff)))))
     print("avg no. trips students: {trips}".format(
-        trips=str(len(students)/ (len(male_students) + len(female_students)))))
+        trips=str(len(students) / (len(male_students) + len(female_students)))))
 
     fig1 = plt.figure()
-    plt.hist([[start.hour for (start, end) in ftrips], [start.hour for (start, end) in mtrips]], 24, normed=True, label=["female", "male"])
-    plt.xticks(range(0,24,2))
+    plt.hist([[start.hour for (start, end) in ftrips], [start.hour for (start, end) in mtrips]], 24, normed=True,
+             label=["female", "male"])
+    plt.xticks(range(0, 24, 2))
     plt.xlabel("hour of day")
     plt.ylabel("probability")
     plt.legend()
     plt.savefig("trip_start_by_gender.png")
 
     fig2 = plt.figure()
-    plt.hist([[start.hour for (start, end) in staff], [start.hour for (start, end) in students]], 24, normed=True, label=["staff/faculty", "students"])
-    plt.xticks(range(0,24,2))
+    plt.hist([[start.hour for (start, end) in staff], [start.hour for (start, end) in students]], 24, normed=True,
+             label=["staff/faculty", "students"])
+    plt.xticks(range(0, 24, 2))
     plt.xlabel("hour of day")
     plt.ylabel("probability")
     plt.legend()
@@ -74,7 +76,7 @@ with mysql.connect(**config["webike.mysql"]) as mysql_client:
 
     fig3 = plt.figure()
     plt.hist([start.hour for (start, end) in ftrips + mtrips], 24, normed=True, rwidth=0.9, label="all participants")
-    plt.xticks(range(0,24,2))
+    plt.xticks(range(0, 24, ))
     plt.xlabel("hour of day")
     plt.ylabel("probability")
     plt.legend()
@@ -84,27 +86,37 @@ with mysql.connect(**config["webike.mysql"]) as mysql_client:
     data = [(end - start).total_seconds() / 60 for (start, end) in ftrips + mtrips]
     print("avg dur trips: {trips}".format(trips=np.mean(data)))
     bins = range(0, int(max(data)) + 5, 5)
-    plt.hist(data, bins=bins, zorder=2, rwidth=0.9, normed=True)
-    plt.hist(data, bins=bins, cumulative=True, zorder=1, rwidth=0.9, normed=True)
-    plt.xlabel("hour of day")
+    plt.hist(data, bins=bins, zorder=2, rwidth=0.9, label="all participants")
+    plt.hist(data, bins=bins, cumulative=True, zorder=1, rwidth=0.9, label="all participants (cum.)")
+    plt.xlabel("average trip duration (min)")
     plt.ylabel("frequency")
+    plt.legend()
     plt.savefig("trip_duration_cum.png")
 
     fig5 = plt.figure()
-    data = [[(end - start).total_seconds() / 60 for (start, end) in trips] for trips in [mtrips, ftrips]]
+    data = [[(end - start).total_seconds() / 60 for (start, end) in trips] for trips in [ftrips, mtrips]]
     print("avg dur trips male: {trips}".format(trips=np.mean(data[0])))
     print("avg dur trips female: {trips}".format(trips=np.mean(data[1])))
-    plt.hist(data, bins=bins, normed=True, zorder=2)
+    plt.hist(data, bins=bins, normed=True, label=["female", "male"])
+    plt.xlabel("average trip duration (min)")
+    plt.ylabel("probability")
+    plt.legend()
     plt.savefig("trip_duration_by_gender.png")
 
     fig6 = plt.figure()
     data = [[(end - start).total_seconds() / 60 for (start, end) in trips] for trips in [staff, students]]
     print("avg dur trips staff: {trips}".format(trips=np.mean(data[0])))
     print("avg dur trips students: {trips}".format(trips=np.mean(data[1])))
-    plt.hist(data, bins=bins, normed=True, zorder=2)
+    plt.hist(data, bins=bins, normed=True, label=["staff/faculty", "students"])
+    plt.xlabel("average trip duration (min)")
+    plt.ylabel("probability")
+    plt.legend()
     plt.savefig("trip_duration_by_occupation.png")
 
     fig7 = plt.figure()
     data = [start.month for (start, end) in ftrips + mtrips]
-    plt.hist(data, bins=12, normed=True, zorder=2, rwidth=0.9)
+    plt.hist(data, bins=12, normed=True, zorder=2, rwidth=0.9, label=["all participants"])
+    plt.xlabel("average trip duration (min)")
+    plt.ylabel("probability")
+    plt.legend()
     plt.savefig("trip_by_month.png")
